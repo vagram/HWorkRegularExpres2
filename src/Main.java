818305f97eab049d9779d54cbb621affc6314ba1
Vagram_ <vdourmychev@gmail.com>
2022-04-13 13:08:38 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,42 @@
 import java.util.Scanner;
-
 public class Main {
-    public static String phoneNumb;
     private static String cleanPhoneNumb;
-
     public static void main(String[] args) {
+        all();
+    }
+
+    public static void all() {
         cleanString();
-        stringLengthLimit10();
+        if (cleanPhoneNumb.length() == 10) {
+            stringLengthLimit10();
+        } else if (cleanPhoneNumb.length() == 11) {
+            stringLengthLimit11();
+        } else {
+            errorPrint();
+            return;
+        }
+        print();
     }
 
     public static void cleanString() {
         System.out.println("Введите номер телефона: ");
-        phoneNumb = new Scanner(System.in).nextLine();
-        cleanPhoneNumb = phoneNumb.replaceAll("\\D", "");
+        cleanPhoneNumb = new Scanner(System.in).nextLine();
+        cleanPhoneNumb = cleanPhoneNumb.replaceAll("\\D", "");
     }
 
     public static void stringLengthLimit10() {
         if (cleanPhoneNumb.length() == 10) {
             cleanPhoneNumb = "7" + cleanPhoneNumb;
-            print();
-            return;
-        } else if (cleanPhoneNumb.length() < 10 || cleanPhoneNumb.length() > 11) {
-            errorPrint();
-        }
-        if (cleanPhoneNumb.length() == 11) {
-            stringLengthLimit11();
         }
     }
-
 
     public static void stringLengthLimit11() {
         if (cleanPhoneNumb.length() == 11) {
             if (cleanPhoneNumb.charAt(0) == '8') {
                 cleanPhoneNumb = cleanPhoneNumb.replaceFirst("[8]", "7");
-                print();
-            } else if (cleanPhoneNumb.charAt(0) == '7') {
-                print();
             }
         }
-        if (cleanPhoneNumb.length() > 11 || cleanPhoneNumb.length() < 10) {
-            errorPrint();
-        }
     }
-
 
     public static void print() {
         System.out.println(cleanPhoneNumb);
@@ -52,4 +46,3 @@ public class Main {
         System.out.println("Неправильный формат ввода");
     }
 }
-
